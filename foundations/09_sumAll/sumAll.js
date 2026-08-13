e5dfaef8fc73d79ad1522a55cc90d13ef017585a
PaginaeDe <220205201+PaginaeDe@users.noreply.github.com>
2026-08-13 10:22:06 +0400

    Solve the sum all exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if ((num1 < 0 || Number.isInteger(num1) === false) || (num2 < 0 || Number.isInteger(num2) === false)) {
+    return "ERROR";
+  };
+  
+  if (num1 > num2) {
+    [num1, num2] = [num2, num1];
+  };
 
+  const rangeOfNumbers = [];
+  for (num1; num1 <= num2; num1++) {
+    rangeOfNumbers.push(num1);
+  };
+
+  return rangeOfNumbers.reduce((total, number) => total += number, 0);
 };
 
-// Do not edit below this line
 module.exports = sumAll;
+
+console.log(sumAll(1, 4));
