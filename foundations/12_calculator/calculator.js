0ad0af82636ce055cc61024582ba119d55beb135
PaginaeDe <220205201+PaginaeDe@users.noreply.github.com>
2026-08-15 13:39:20 +0400

    Solve the calculator exercise

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(toSum) {
+  return toSum.reduce((total, number) => total + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(toMultiply) {
+  return toMultiply.reduce((total, number) => total * number, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base**exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let product = 1;
+	for (let i = 2; i < number+1; i++) {
+    product *= i;
+  };
+  return product;
 };
 
 // Do not edit below this line
@@ -31,3 +35,5 @@ module.exports = {
   power,
   factorial
 };
+
+console.log(sum([2,43,43]));
