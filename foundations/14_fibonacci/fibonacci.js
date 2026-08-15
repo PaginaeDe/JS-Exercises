f6b6cad64483e5ec93dafd854376016e4e8249a3
PaginaeDe <220205201+PaginaeDe@users.noreply.github.com>
2026-08-15 14:09:56 +0400

    Solve the fibonacci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(nthTerm) {
+  const fibonacciList = [0, 1];
+  if (nthTerm < 0) return "OOPS"
+  else if (nthTerm <= 1) return fibonacciList[nthTerm];
 
+  for (i = 0; i < nthTerm-1; i++) {
+    fibonacciList.push(fibonacciList[fibonacciList.length-1] + fibonacciList[fibonacciList.length-2])
+  };
+
+  return fibonacciList[nthTerm];
 };
 
-// Do not edit below this line
 module.exports = fibonacci;
