b0660cdf101042ba33a80d9f6d56e3df822ee851
PaginaeDe <220205201+PaginaeDe@users.noreply.github.com>
2026-08-15 13:57:01 +0400

    Solve the palindrome exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const allowedChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+  string = String(string);
+  const parsedString = string.toLowerCase()
+        .split("")
+        .filter(char => allowedChar.includes(char))
+        .join();
+  const reversedString = parsedString.split("").reverse().join("");
+  return parsedString === reversedString ? true : false;
 };
 
-// Do not edit below this line
 module.exports = palindromes;
+
+console.log(palindromes('r3ace3car'));
